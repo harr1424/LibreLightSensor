47b383e0634afb9ee7c35a0594165c15984be5d2
harr1424 <harrington59802@protonmail.com>
2022-09-03 16:36:26 -0600

    Initial Commit

---
+++ LibreLightSensor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LibreLightSensor
+//
+//  Created by user on 9/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LibreLightSensor/LibreLightSensorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibreLightSensorApp.swift
+//  LibreLightSensor
+//
+//  Created by user on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LibreLightSensorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
